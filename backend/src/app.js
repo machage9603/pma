2234fd3d075c7a46e32834830e75258e0f39487a
machage9603 <mikemachage@gmail.com>
2024-12-21 18:37:30 +0300

    add backend configuration files

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Project Management API' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+module.exports = app;
