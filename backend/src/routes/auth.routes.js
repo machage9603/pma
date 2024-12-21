e46b2b241544aed0f623ea1f82859c857c808531
machage9603 <mikemachage@gmail.com>
2024-12-21 20:19:20 +0300

    add auth routes

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+// backend/src/routes/auth.routes.js
+const express = require('express');
+const { register, login, getProfile } = require('../controllers/auth.controller');
+const { protect } = require('../middleware/auth.middleware');
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/profile', protect, getProfile);
+
+module.exports = router;
